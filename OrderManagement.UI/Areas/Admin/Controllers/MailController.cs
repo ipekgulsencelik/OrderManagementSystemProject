0ea8c2286bf76a500d470a41b7afbc0a53322505
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-01-03 23:59:03 +0300

    Mail

---
+++ OrderManagement.UI/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using OrderManagement.UI.DTOs.MailDTOs;
+using MailKit.Net.Smtp;
+
+namespace OrderManagement.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class MailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateMailDTO createMailDTO)
+        {
+            var mimeMessage = new MimeMessage();
+
+            var mailBoxAddressFrom = new MailboxAddress("M&Y Restoran Rezervasyon", "mltmcelik93@gmail.com");
+            mimeMessage.From.Add(mailBoxAddressFrom);
+
+            var mailBoxAddressTo = new MailboxAddress("Sayın Kullanıcı", createMailDTO.ReceiverMail);
+            mimeMessage.To.Add(mailBoxAddressTo);
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = createMailDTO.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = createMailDTO.Subject;
+            var client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("mltmcelik93@gmail.com", "ndcgzpgkdrfiipmc");
+
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return NoContent();
+        }
+    }
+}
---
+++ OrderManagement.UI/DTOs/MailDTOs/CreateMailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderManagement.UI.DTOs.MailDTOs
+{
+    public class CreateMailDTO
+    {
+        public string? ReceiverMail { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
