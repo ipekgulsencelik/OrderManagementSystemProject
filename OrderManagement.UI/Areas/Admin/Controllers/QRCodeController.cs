dba0f7760b919ac671dbdd7f309c1829f41cca35
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-01-03 22:36:49 +0300

    QR Code

---
+++ OrderManagement.UI/Areas/Admin/Controllers/QRCodeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Text;
+using ZXing.Windows.Compatibility;
+using ZXing;
+
+
+namespace OrderManagement.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class QRCodeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public QRCodeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string value)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var codeGenerator = new QRCodeGenerator();
+                QRCodeGenerator.QRCode squareCode = codeGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                using (Bitmap image = squareCode.GetGraphic(10))
+                {
+                    string webRootPath = _environment.WebRootPath;
+                    image.Save(webRootPath + "\\QRimages\\QrCode.png");
+                    ViewBag.qrCodeImage = "\\QRimages\\QrCode.png";
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Decode(string code)
+        {
+            string webRootPath = _environment.WebRootPath;
+            var path = webRootPath + "\\QRimages\\QrCode.png";
+            var reader = new BarcodeReaderGeneric();
+            Bitmap image = (Bitmap)Image.FromFile(path);
+            using (image)
+            {
+                LuminanceSource source;
+                source = new BitmapLuminanceSource(image);
+                var result = reader.Decode(source);
+                byte[] bytes = Encoding.Default.GetBytes(result.ToString());
+                string myString = Encoding.UTF8.GetString(bytes);
+
+                ViewBag.qrText = myString;
+                TempData["qrTableNumber"] = myString;
+            }
+            return View("Index");
+        }
+    }
+}
